335332a122064031cb10deb182663353d25276ab
philippe-hjik <122774959+philippe-hjik@users.noreply.github.com>
2024-03-25 13:47:11 +0100

    correction des models dans sequelize avec longblob

--- PassionLivre/model/t_books.mjs
+++ PassionLivre/model/t_books.mjs
@@ -2,13 +2,13 @@ const bookModel = (sequelize, DataTypes) => {
     return sequelize.define(
       "t_books",
       {
-        id_book: {
+        id: {
           type: DataTypes.INTEGER,
           primaryKey: true,
           autoIncrement: true,
         },
-        title_book: {
-          type: DataTypes.STRING,
+        epub: {
+          type: DataTypes.BLOB('long'),
           allowNull: false,
           validate: {
             notEmpty: {
@@ -18,113 +18,7 @@ const bookModel = (sequelize, DataTypes) => {
               msg: "Le nom est une propriété obligatoire.",
             },
           },
-        },
-        pages_book: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                is: {
-                    args: /^[0-9]\d+$/,
-                    msg: "Seules les lettres et les espaces sont autorisées.",
-                },
-                notEmpty: {
-                  msg: "Le nom ne peut pas être vide.",
-                },
-            },
-        },
-        extract_book: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            validate: {
-                notEmpty: {
-                  msg: "Le nom ne peut pas être vide.",
-                },
-            },
-        },
-        summary_book: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            validate: {
-                notEmpty: {
-                  msg: "Le nom ne peut pas être vide.",
-                },
-            },
-        },
-        cover_book: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            validate: {
-                notEmpty: {
-                    msg: "Le nom ne peut pas être vide.",
-                },
-            }
-        },
-        year_book: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            validate: {
-                is: {
-                    args: /^[0-9]\d+$/,
-                    msg: "Seules les chiffre et les espaces sont autorisées.",
-                },
-                notEmpty: {
-                  msg: "Le nom ne peut pas être vide.",
-                },
-            }
-        },
-        average_book: {
-            type: DataTypes.FLOAT,
-            allowNull: true,
-        },
-        upload_book: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            validate: {
-                notEmpty: {
-                    msg: "Le nom ne peut pas être vide.",
-                },
-            }
-        },
-        fk_publisher: { // Champ pour la clé étrangère
-            type: DataTypes.INTEGER, // Le type doit correspondre à celui de la clé primaire dans la table des auteurs
-            allowNull: true,
-            references: {
-              model: 't_publishers', // Nom de la table que vous référencez
-              key: 'id_publisher' // Nom de la colonne que vous référencez dans la table des auteurs
-            },
-            onUpdate: 'CASCADE', // Options pour le comportement de mise à jour et de suppression
-            onDelete: 'RESTRICT'
-        },
-        fk_author: { // Champ pour la clé étrangère
-            type: DataTypes.INTEGER, // Le type doit correspondre à celui de la clé primaire dans la table des auteurs
-            allowNull: true,
-            references: {
-              model: 't_authors', // Nom de la table que vous référencez
-              key: 'id_author' // Nom de la colonne que vous référencez dans la table des auteurs
-            },
-            onUpdate: 'CASCADE', // Options pour le comportement de mise à jour et de suppression
-            onDelete: 'RESTRICT'
-        },
-        fk_category: { // Champ pour la clé étrangère
-            type: DataTypes.INTEGER, // Le type doit correspondre à celui de la clé primaire dans la table des auteurs
-            allowNull: true,
-            references: {
-              model: 't_categories', // Nom de la table que vous référencez
-              key: 'id_category' // Nom de la colonne que vous référencez dans la table des auteurs
-            },
-            onUpdate: 'CASCADE', // Options pour le comportement de mise à jour et de suppression
-            onDelete: 'RESTRICT'
-        },
-        fk_user: { // Champ pour la clé étrangère
-            type: DataTypes.INTEGER, // Le type doit correspondre à celui de la clé primaire dans la table des auteurs
-            allowNull: true,
-            references: {
-              model: 't_users', // Nom de la table que vous référencez
-              key: 'id_user' // Nom de la colonne que vous référencez dans la table des auteurs
-            },
-            onUpdate: 'CASCADE', // Options pour le comportement de mise à jour et de suppression
-            onDelete: 'RESTRICT'
-        },
+        }
       },
       {
         timestamps: true,
--- PassionLivre/model/t_categories.mjs
+++ PassionLivre/model/t_categories.mjs
@@ -2,22 +2,14 @@ const categoryModel = (sequelize, DataTypes) => {
     return sequelize.define(
       "t_categories",
       {
-        id_category: {
+        id: {
           type: DataTypes.INTEGER,
           primaryKey: true,
           autoIncrement: true,
         },
-        name_category: {
+        tag: {
             type: DataTypes.STRING,
             allowNull: false,
-            validate: {
-                notEmpty: {
-                  msg: "Le nom ne peut pas être vide.",
-                }
-                // notNull: {
-                //   msg: "Le nom est une propriété obligatoire.",
-                // },
-            },
         },
       }
     );
